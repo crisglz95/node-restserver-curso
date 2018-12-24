f7a6f8d458c21014f497be5e7f7d589c2cc4fc48
Cris Glz <the.joker.cr9@gmail.com>
2018-12-24 12:39:44 -0600

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,15 @@
 //Puerto
 //===================================
 process.env.PORT = process.env.PORT || 3000;
+
+//====================================
+//Vencimiento del token
+//===================================
+//60 seg * 60 min * 24 hrs * 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//====================================
+//SEED
+//===================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
