262df6b95f9d6a7e741cc1f18e1f0d91eff3b9b3
Cris Glz <the.joker.cr9@gmail.com>
2019-02-08 10:46:23 -0600

    Productos y Categorias

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ process.env.PORT = process.env.PORT || 3000;
 //Vencimiento del token
 //===================================
 //60 seg * 60 min * 24 hrs * 30 dias
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48hr';
 
 
 //====================================
